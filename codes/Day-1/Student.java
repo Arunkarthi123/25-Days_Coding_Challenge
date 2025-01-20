c7933f2c47e9aa8a09926c646a309d3eda90bc25
Arunkarthick <arunkarthi9994@gmail.com>
2025-01-20 15:58:27 +0530

    file modified

--- codes/Day-1/Student.java
+++ codes/Day-1/Student.java
@@ -5,14 +5,20 @@ public class Student
     public int mark;
     void display()
     {
-        System.out.println("name: "+name+" roll num : "+rollnum+" marks :"+mark);
+        System.out.println("name: "+name+" \nroll num : "+rollnum+" \nmarks :"+mark);
+    }
+    Student(String n , int m , int rn)
+    {
+        this.name=n;
+        this.mark=m;
+        this.rollnum=rn;
     }
     static public void main(String [] args)
     {
-        Student s1 = new Student();
-        System.out.println(s1);
-        Student s2 = new Student();
-        System.out.println(s2);
+        Student s1 = new Student("Arun",100,77);
+        s1.display();
+        Student s2 = new Student("Dhandu",95,309);
+        s2.display();
     }
 
 }
