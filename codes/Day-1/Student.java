2d427d876063eb13338a9c6905f40fff503a451c
Arunkarthick <arunkarthi9994@gmail.com>
2025-01-20 12:07:10 +0530

    File updated

--- codes/Day-1/Student.java
+++ codes/Day-1/Student.java
@@ -1,7 +1,18 @@
 public class Student
 {
-    static void main(String [] arun)
+    public String name;
+    public int rollnum;
+    public int mark;
+    void display()
     {
-        
+        System.out.println("name: "+name+" roll num : "+rollnum+" marks :"+mark);
     }
+    static public void main(String [] args)
+    {
+        Student s1 = new Student();
+        System.out.println(s1);
+        Student s2 = new Student();
+        System.out.println(s2);
+    }
+
 }
